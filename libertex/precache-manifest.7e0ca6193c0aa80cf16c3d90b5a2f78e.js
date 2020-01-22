d19906bb814b2fccdfb6d1ec04a12aeb3a6b86f8
haasson <haasson22@gmail.com>
2020-01-22 09:49:17 +0700

    initial commit

---
+++ libertex/precache-manifest.7e0ca6193c0aa80cf16c3d90b5a2f78e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c93f5ff9460f404b3de",
+    "url": "/libertex/css/app.ff685c6b.css"
+  },
+  {
+    "revision": "12c6e4302f2c8d61c5e425e8626f324e",
+    "url": "/libertex/fonts/ArialMT.12c6e430.woff"
+  },
+  {
+    "revision": "9a3bf7acae14d9b5ed5a88458106b58b",
+    "url": "/libertex/fonts/Roboto-Medium.9a3bf7ac.woff"
+  },
+  {
+    "revision": "b4d019ac3de354fba15fe1ad4a0637a7",
+    "url": "/libertex/img/arrow.b4d019ac.svg"
+  },
+  {
+    "revision": "4c6c9c5d512a61dac65bcc7bfb18240c",
+    "url": "/libertex/index.html"
+  },
+  {
+    "revision": "1c93f5ff9460f404b3de",
+    "url": "/libertex/js/app.48c7fb8d.js"
+  },
+  {
+    "revision": "5f221339fcf4051febc1",
+    "url": "/libertex/js/chunk-vendors.09993f0c.js"
+  },
+  {
+    "revision": "569b48eaa2da928ec43629533f5063e6",
+    "url": "/libertex/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/libertex/robots.txt"
+  }
+]);
---
+++ libertex/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/libertex/precache-manifest.7e0ca6193c0aa80cf16c3d90b5a2f78e.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "libertex"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
